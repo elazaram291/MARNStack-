01d555df6dbd5305eba3fb7f7424255154846e0e
Elazar <elazaram291@gmail.com>
2023-10-05 14:21:54 +0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import AppRouters from "./AppRouters";
+import { MyContext } from "./context/MyContext";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MyContext.Provider value={{}}>
+      <AppRouters />
+      <ToastContainer theme="colored" />
+    </MyContext.Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Home_P.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Home_P() {
+  return (
+    <div>Home_P</div>
+  )
+}
